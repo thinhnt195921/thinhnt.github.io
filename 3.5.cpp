be219fca6377ba061355d3541cd1cf91628afc66
thinhnt195921 <65150141+thinhnt195921@users.noreply.github.com>
2020-05-25 14:02:21 +0700

    Add files via upload

---
+++ 3.5.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <conio.h>
+main()
+{float X,Y;
+scanf("%f",&X);
+scanf("%f",&Y);
+printf("%f",X*X+Y*Y);
+printf("\n%f",(X+Y)*(X+Y));
+printf("\n%f",(X-Y)*(X-Y));
+getch();
+}
